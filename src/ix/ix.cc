6c5261e06b6227167765b7c468f0c8715a2b7eaa
d4rkb1ue <7874364@gmail.com>
2017-11-18 16:30:13 -0800

    feature: init new tree with one insert; pass t2

--- src/ix/ix.cc
+++ src/ix/ix.cc
@@ -53,7 +53,15 @@ RC IndexManager::closeFile(IXFileHandle &ixfileHandle)
 
 RC IndexManager::insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid)
 {
-    return -1;
+    // TODO: only care about int/real
+    unsigned len = 4;
+    char _key[4];
+    const char *ckey = static_cast<const char *>(key);
+    for (int i = 0; i < len; i++)
+    {
+        _key[i] = ckey[i];
+    }
+    return ixfileHandle.getTree(attribute.type)->insert(_key, len, rid);
 }
 
 RC IndexManager::deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid)
@@ -74,6 +82,8 @@ RC IndexManager::scan(IXFileHandle &ixfileHandle,
 
 void IndexManager::printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const
 {
+    cout << endl
+         << ixfileHandle.getTree(attribute.type)->toString() << endl;
 }
 
 /****************************************************
@@ -101,48 +111,80 @@ RC IX_ScanIterator::close()
  *                  IXFileHandle                    *
  ****************************************************/
 IXFileHandle::IXFileHandle()
+    : tree(nullptr),
+      _pfm(PagedFileManager::instance()),
+      fileName(""),
+      ixReadPageCounter(0),
+      ixWritePageCounter(0),
+      ixAppendPageCounter(0)
 {
-    ixReadPageCounter = 0;
-    ixWritePageCounter = 0;
-    ixAppendPageCounter = 0;
-    fileName = "";
 }
-
-IXFileHandle::IXFileHandle(string indexFileName)
+IXFileHandle::IXFileHandle(string fileName)
+    : tree(nullptr),
+      _pfm(PagedFileManager::instance()),
+      fileName(fileName),
+      ixReadPageCounter(0),
+      ixWritePageCounter(0),
+      ixAppendPageCounter(0)
 {
-    ixReadPageCounter = 0;
-    ixWritePageCounter = 0;
-    ixAppendPageCounter = 0;
-    if (!_pfm)
-    {
-        _pfm = PagedFileManager::instance();
-    }
-
-    if (_pfm->openFile(indexFileName, _fileHandle) != 0)
+    if (_pfm->openFile(fileName, _fileHandle) != 0)
     {
         cerr << "Open index file failed." << endl;
         exit(-1);
     }
-
-    fileName = indexFileName;
 }
 
-RC IXFileHandle::writePage(PageNum pageNum, const void *data, unsigned dataSize)
+IXFileHandle::~IXFileHandle()
+{
+    if (tree)
+    {
+        delete tree;
+    }
+}
+
+BTree *IXFileHandle::getTree(AttrType attrType)
+{
+    if (!tree)
+    {
+        tree = new BTree(this, attrType);
+    }
+    return tree;
+}
+
+RC IXFileHandle::writePage(PageNum pageNum, char *data)
 {
     ixWritePageCounter++;
-    return _fileHandle.writePage(pageNum, data, dataSize);
+    return _fileHandle.writePage(pageNum, data);
 }
 
-RC IXFileHandle::appendPage(const void *data, unsigned dataSize)
+RC IXFileHandle::appendPage(char *data)
+{
+    unsigned pageNum = 0;
+    return appendPage(data, pageNum);
+}
+
+RC IXFileHandle::appendPage(char *data, PageNum &pageNum)
 {
     ixAppendPageCounter++;
-    return _fileHandle.appendPage(data, dataSize);
+    if (_fileHandle.appendPage(data) != 0)
+    {
+        cerr << "append page in index file handle failed." << endl;
+        exit(-1);
+    }
+    // the new page is the last page
+    pageNum = getNumberOfPages() - 1;
+    return 0;
 }
 
-RC IXFileHandle::readPage(PageNum pageNum, void *data)
+RC IXFileHandle::readPage(PageNum pageNum, char *data)
 {
     ixReadPageCounter++;
     return _fileHandle.readPage(pageNum, data);
+}
+
+unsigned IXFileHandle::getNumberOfPages()
+{
+    return _fileHandle.getNumberOfPages();
 }
 
 RC IXFileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount)
@@ -156,4 +198,405 @@ RC IXFileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePa
 RC IXFileHandle::closeFile()
 {
     return _fileHandle.close();
+}
+
+/****************************************************
+ *                       BTree                      *
+ ****************************************************/
+BTree::BTree(IXFileHandle *fileHandle, AttrType attrType)
+    : _fileHandle(fileHandle),
+      attrType(attrType)
+{
+    if (_fileHandle->getNumberOfPages() == 0)
+    {
+        root = nullptr;
+        rootPn = 0;
+        return;
+    }
+    cerr << "first insert shouldn't read any page." << endl;
+    exit(-1);
+    // // if file isn't empty, read meta page
+    // _fileHandle->readPage(0, buffer);
+    // MetaPage meta(buffer);
+    // rootPn = meta.rootPn;
+
+    // // read root node
+    // _fileHandle->readPage(rootPn, buffer);
+    // if (meta.rootIsLeaf)
+    // {
+    //     root = new LeafPage(buffer);
+    // }
+    // else
+    // {
+    //     root = new InternalPage(buffer);
+    // }
+}
+
+BTree::~BTree()
+{
+    if (root)
+    {
+        if (root->isLeaf)
+        {
+            delete static_cast<LeafPage *>(root);
+        }
+        else
+        {
+            delete static_cast<InternalPage *>(root);
+        }
+        root = nullptr;
+    }
+}
+
+bool BTree::isEmpty()
+{
+    return !root;
+}
+
+// insert
+
+RC BTree::insert(char *key, unsigned len, RID rid)
+{
+    if (isEmpty())
+    {
+        return initNewTree(key, len, rid);
+    }
+    return insertToLeaf(key, len, rid);
+}
+
+RC BTree::initNewTree(char *key, unsigned len, RID rid)
+{
+    // appent meta page before append a leaf page
+    updateRoot();
+
+    LeafPage *node = new LeafPage(attrType, nullptr);
+    node->insert(key, len, rid);
+    root = node;
+
+    // persist
+    if (node->getRawData(buffer) != 0)
+    {
+        cerr << "when get raw data from node, size > PAGE_SIZE" << endl;
+        exit(-1);
+    }
+    if (_fileHandle->appendPage(buffer, rootPn) != 0)
+    {
+        cerr << "append page fail when init new tree." << endl;
+        exit(-1);
+    }
+
+    if (rootPn != 1)
+    {
+        cerr << endl
+             << "new leaf page's num should be 1." << endl;
+        exit(-1);
+    }
+
+    // update root pageNum in meta page
+    updateRoot();
+    return 0;
+}
+
+RC BTree::insertToLeaf(char *key, unsigned len, RID rid)
+{
+    return -1;
+}
+
+void BTree::updateRoot()
+{
+    // no meta page, no root
+    if (!root)
+    {
+        if (_fileHandle->getNumberOfPages() > 0)
+        {
+            cerr << "append page when no root set, but already has some pages?" << endl;
+            exit(-1);
+        }
+
+        // insert an pure 0 page
+        memset(buffer, 0, PAGE_SIZE);
+        _fileHandle->appendPage(buffer);
+        return;
+    }
+
+    MetaPage meta(rootPn, root->isLeaf);
+    meta.getRawData(buffer);
+    _fileHandle->writePage(0, buffer);
+}
+
+// print
+
+string BTree::toString()
+{
+    if (isEmpty())
+    {
+        return "[0_EMPTY_TREE_0]";
+    }
+    string s = "[" + root->toString() + "]";
+    return s;
+}
+
+/****************************************************
+ *                    MetaPage                      *
+ ****************************************************/
+MetaPage::MetaPage(PageNum rootPn, bool rootIsLeaf)
+    : rootPn(rootPn),
+      rootIsLeaf(rootIsLeaf)
+{
+}
+
+MetaPage::MetaPage(char *rawData)
+{
+    memcpy(&rootPn, rawData, sizeof(unsigned));
+    rawData += sizeof(unsigned);
+    memcpy(&rootIsLeaf, rawData, sizeof(bool));
+}
+
+RC MetaPage::getRawData(char *data)
+{
+    memset(data, 0, PAGE_SIZE);
+    memcpy(data, &rootPn, sizeof(unsigned));
+    data += sizeof(unsigned);
+    memcpy(data, &rootIsLeaf, sizeof(bool));
+    return 0;
+}
+
+/****************************************************
+ *                    NodePage                      *
+ ****************************************************/
+NodePage::NodePage(NodePage *parent, AttrType attrType, unsigned size, bool isLeaf)
+    : parent(parent),
+      isRoot(parent == nullptr),
+      isLeaf(isLeaf),
+      attrType(attrType),
+      size(size)
+{
+}
+
+/****************************************************
+ *                  InternalPage                    *
+ ****************************************************/
+InternalPage::InternalPage(AttrType attrType, InternalPage *parent)
+    : NodePage(parent, attrType, 0, false)
+{
+}
+InternalPage::InternalPage(char *rawData, AttrType attrType, InternalPage *parent)
+    : NodePage(parent, attrType, 0, false)
+{
+    cerr << "should not make internal now.." << endl;
+    exit(-1);
+}
+
+InternalPage::~InternalPage()
+{
+    for (int i = 0; i < entries.size(); i++)
+    {
+        delete entries[i];
+    }
+}
+
+RC InternalPage::getRawData(char *data)
+{
+    return -1;
+}
+
+string InternalPage::toString()
+{
+    return "TODO";
+}
+
+/****************************************************
+ *                    LeafPage                      *
+ ****************************************************
+ *  
+ *  [isLeaf][next leaf pageNum][entries num][entries...]*
+ * 
+ */
+LeafPage::LeafPage(AttrType attrType, InternalPage *parent)
+    : NodePage(parent, attrType, LEAF_PAGE_HEADER_SIZE, true),
+      nextPn(0)
+{
+}
+LeafPage::LeafPage(char *rawData, AttrType attrType, InternalPage *parent)
+    : NodePage(parent, attrType, LEAF_PAGE_HEADER_SIZE, true)
+{
+    cerr << "should not read leaf page from disk now" << endl;
+    exit(-1);
+    // unsigned entriesNum = 0;
+
+    // memcpy(&isLeaf, rawData, sizeof(bool));
+    // rawData += sizeof(bool);
+    // memcpy(&nextPn, rawData, sizeof(PageNum));
+    // rawData += sizeof(PageNum);
+    // memcpy(&entriesNum, rawData, sizeof(unsigned));
+    // rawData += sizeof(unsigned);
+
+    // for (int i = 0; i < entriesNum; i++)
+    // {
+    //     entries.push_bask(new LeafEntry())
+    // }
+}
+
+LeafPage::~LeafPage()
+{
+    for (int i = 0; i < entries.size(); i++)
+    {
+        delete entries[i];
+    }
+}
+
+RC LeafPage::lookupAndInsert(char *key, unsigned len, RID rid)
+{
+    return -1;
+}
+
+LeafEntry *LeafPage::lookup(char *key)
+{
+    return nullptr;
+}
+
+RC LeafPage::insert(char *key, unsigned len, RID rid)
+{
+    LeafEntry *e = new LeafEntry(key, len, rid);
+    auto it2ptr = entries.begin();
+    auto end = entries.end();
+    for (; it2ptr != end && e->compareTo(*it2ptr, attrType) > 0; it2ptr++)
+    {
+    }
+    entries.insert(it2ptr, e);
+    size += len + sizeof(RID);
+    return 0;
+}
+
+RC LeafPage::getRawData(char *data)
+{
+    if (tooBig())
+    {
+        return -1;
+    }
+
+    unsigned entryNum = entries.size();
+    memset(data, 0, PAGE_SIZE);
+
+    memcpy(data, &isLeaf, sizeof(bool));
+    data += sizeof(bool);
+    memcpy(data, &nextPn, sizeof(unsigned));
+    data += sizeof(unsigned);
+    memcpy(data, &entryNum, sizeof(unsigned));
+    data += sizeof(unsigned);
+
+    for (vector<LeafEntry *>::iterator it = entries.begin(); it != entries.end(); it++)
+    {
+        memcpy(data, (*it)->key, (*it)->size);
+        data += (*it)->size;
+        memcpy(data, &((*it)->rid), sizeof(RID));
+        data += sizeof(RID);
+    }
+    return 0;
+}
+
+string LeafPage::toString()
+{
+    string s = "[";
+    for (int i = 0; i < entries.size(); i++)
+    {
+        s += entries[i]->toString(attrType) + ", ";
+    }
+    s = s.substr(0, s.size() - 2);
+    s += "]";
+    return s;
+}
+
+/****************************************************
+ *                  InternalEntry                   *
+ ****************************************************/
+InternalEntry::InternalEntry(char *key, unsigned len, PageNum ptrNum)
+    : size(len),
+      ptrNum(ptrNum)
+{
+    this->key = new char[size];
+    memcpy(this->key, key, size);
+}
+
+InternalEntry::~InternalEntry()
+{
+    if (key)
+    {
+        delete[] key;
+    }
+}
+
+/****************************************************
+ *                    LeafEntry                     *
+ ****************************************************/
+LeafEntry::LeafEntry(char *key, unsigned len, RID rid, bool isDeleted)
+    : size(len),
+      rid(rid),
+      isDeleted(isDeleted)
+{
+    this->key = new char[size];
+    memcpy(this->key, key, size);
+}
+
+LeafEntry::~LeafEntry()
+{
+    if (key)
+    {
+        delete[] key;
+    }
+}
+
+int LeafEntry::compareTo(LeafEntry *that, AttrType attrType)
+{
+    switch (attrType)
+    {
+    case TypeInt:
+    {
+        int int_this = 0, int_that = 0;
+        memcpy(&int_this, key, sizeof(int));
+        memcpy(&int_that, that->key, sizeof(int));
+        return int_this - int_that;
+    }
+    case TypeReal:
+    {
+        float f_this = 0, f_that = 0;
+        memcpy(&f_this, key, sizeof(float));
+        memcpy(&f_that, that->key, sizeof(float));
+        if (f_this - f_that < 0.000001)
+        {
+            return 0;
+        }
+        return f_this - f_that < 0 ? -1 : 1;
+    }
+    case TypeVarChar:
+    {
+        cerr << "TODO shouldn't compare VarChar now" << endl;
+        exit(-1);
+    }
+    }
+    return 0;
+}
+
+string LeafEntry::toString(AttrType attrType)
+{
+    string s_rid = "(" + to_string(rid.pageNum) + "," + to_string(rid.slotNum) + ")";
+    switch (attrType)
+    {
+    case TypeInt:
+    {
+        int _int = 0;
+        memcpy(&_int, key, sizeof(int));
+        return to_string(_int) + ":" + s_rid;
+    }
+    case TypeReal:
+    {
+        float _f = 0.0;
+        memcpy(&_f, key, sizeof(float));
+        return to_string(_f) + ":" + s_rid;
+    }
+    case TypeVarChar:
+    {
+        return "[varchar..]";
+    }
+    }
 }
--- src/ix/ix.h
+++ src/ix/ix.h
@@ -11,52 +11,59 @@
 
 class IX_ScanIterator;
 class IXFileHandle;
+class BTree;
+class MetaPage;
+class NodePage;
+class InternalPage;
+class LeafPage;
+class InternalEntry;
+class LeafEntry;
 
 /****************************************************
  *                  IndexManager                    *
  ****************************************************/
 class IndexManager
 {
-  private:
-    PagedFileManager *_pfm;
-    static IndexManager *_index_manager;
+private:
+  PagedFileManager *_pfm;
+  static IndexManager *_index_manager;
 
-  protected:
-    IndexManager();
-    ~IndexManager();
+protected:
+  IndexManager();
+  ~IndexManager();
 
-  public:
-    static IndexManager *instance();
+public:
+  static IndexManager *instance();
 
-    // Create an index file.
-    RC createFile(const string &fileName);
+  // Create an index file.
+  RC createFile(const string &fileName);
 
-    // Delete an index file.
-    RC destroyFile(const string &fileName);
+  // Delete an index file.
+  RC destroyFile(const string &fileName);
 
-    // Open an index and return an ixfileHandle.
-    RC openFile(const string &fileName, IXFileHandle &ixfileHandle);
+  // Open an index and return an ixfileHandle.
+  RC openFile(const string &fileName, IXFileHandle &ixfileHandle);
 
-    // Close an ixfileHandle for an index.
-    RC closeFile(IXFileHandle &ixfileHandle);
+  // Close an ixfileHandle for an index.
+  RC closeFile(IXFileHandle &ixfileHandle);
 
-    // Insert an entry into the given index that is indicated by the given ixfileHandle.
-    RC insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
+  // Insert an entry into the given index that is indicated by the given ixfileHandle.
+  RC insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
 
-    // Delete an entry from the given index that is indicated by the given ixfileHandle.
-    RC deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
+  // Delete an entry from the given index that is indicated by the given ixfileHandle.
+  RC deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
 
-    // Initialize and IX_ScanIterator to support a range search
-    RC scan(IXFileHandle &ixfileHandle,
-            const Attribute &attribute,
-            const void *lowKey,
-            const void *highKey,
-            bool lowKeyInclusive,
-            bool highKeyInclusive,
-            IX_ScanIterator &ix_ScanIterator);
+  // Initialize and IX_ScanIterator to support a range search
+  RC scan(IXFileHandle &ixfileHandle,
+          const Attribute &attribute,
+          const void *lowKey,
+          const void *highKey,
+          bool lowKeyInclusive,
+          bool highKeyInclusive,
+          IX_ScanIterator &ix_ScanIterator);
 
-    // Print the B+ tree in pre-order (in a JSON record format)
-    void printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const;
+  // Print the B+ tree in pre-order (in a JSON record format)
+  void printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const;
 };
 
 /****************************************************
@@ -64,15 +71,15 @@ class IndexManager
  ****************************************************/
 class IX_ScanIterator
 {
-  public:
-    IX_ScanIterator();
-    ~IX_ScanIterator();
+public:
+  IX_ScanIterator();
+  ~IX_ScanIterator();
 
-    // Get next matching entry
-    RC getNextEntry(RID &rid, void *key);
+  // Get next matching entry
+  RC getNextEntry(RID &rid, void *key);
 
-    // Terminate index scan
-    RC close();
+  // Terminate index scan
+  RC close();
 };
 
 /****************************************************
@@ -80,25 +87,174 @@ class IX_ScanIterator
  ****************************************************/
 class IXFileHandle
 {
-  private:
-    FileHandle _fileHandle;
-    PagedFileManager *_pfm;
+private:
+  BTree *tree;
 
-  public:
-    string fileName;
-    unsigned ixReadPageCounter;
-    unsigned ixWritePageCounter;
-    unsigned ixAppendPageCounter;
+public:
+  FileHandle _fileHandle;
+  PagedFileManager *_pfm;
+  string fileName;
 
-    RC writePage(PageNum pageNum, const void *data, unsigned dataSize);
-    RC appendPage(const void *data, unsigned dataSize);
-    RC readPage(PageNum pageNum, void *data);
+  unsigned ixReadPageCounter;
+  unsigned ixWritePageCounter;
+  unsigned ixAppendPageCounter;
 
-    RC closeFile();
+  IXFileHandle();
+  IXFileHandle(string fileName);
+  ~IXFileHandle();
 
-    IXFileHandle();
-    IXFileHandle(string indexFileName);
-    ~IXFileHandle(){};
-    RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount);
+  BTree *getTree(AttrType type);
+  // FileHandle won't care the data size anymore, the tree itself can take care
+  RC writePage(PageNum pageNum, char *data);
+  RC appendPage(char *data);
+  RC appendPage(char *data, PageNum &pageNum);
+  RC readPage(PageNum pageNum, char *data);
+  unsigned getNumberOfPages();
+  RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount);
+  RC closeFile();
 };
+
+/****************************************************
+ *                       BTree                      *
+ ****************************************************/
+class BTree
+{
+private:
+  char buffer[PAGE_SIZE];
+
+public:
+  NodePage *root;
+  PageNum rootPn;
+  IXFileHandle *_fileHandle;
+  AttrType attrType;
+
+  BTree(IXFileHandle *fileHandle, AttrType attrType);
+  ~BTree();
+
+  bool isEmpty();
+  RC insert(char *key, unsigned len, RID rid);
+  // RC lazyRemove(char *key, unsigned len);
+  // RC toJSON();
+  string toString();
+
+private:
+  RC initNewTree(char *key, unsigned len, RID rid);
+  RC insertToLeaf(char *key, unsigned len, RID rid);
+  void updateRoot();
+  // RC insertToParent();
+};
+
+/****************************************************
+ *                    MetaPage                      *
+ ****************************************************/
+class MetaPage
+{
+public:
+  PageNum rootPn;
+  bool rootIsLeaf;
+
+  MetaPage(char *rawData);
+  MetaPage(PageNum rootPn, bool rootIsLeaf);
+  ~MetaPage(){};
+
+  RC getRawData(char *data);
+};
+
+/****************************************************
+ *                    NodePage                      *
+ ****************************************************/
+class NodePage
+{
+public:
+  NodePage *parent;
+  bool isRoot;
+  bool isLeaf;
+  AttrType attrType;
+  unsigned size;
+
+  NodePage(NodePage *parent, AttrType attrType, unsigned size, bool isLeaf);
+  virtual ~NodePage(){};
+
+  bool tooBig() { return size > PAGE_SIZE; };
+  virtual RC getRawData(char *data) = 0;
+  virtual string toString() = 0;
+};
+
+/****************************************************
+ *                  InternalPage                    *
+ ****************************************************/
+class InternalPage : public NodePage
+{
+private:
+  vector<InternalEntry *> entries;
+
+public:
+  InternalPage(AttrType attrType, InternalPage *parent);
+  InternalPage(char *rawData, AttrType attrType, InternalPage *parent);
+  ~InternalPage();
+
+  RC getRawData(char *data) override;
+  string toString() override;
+};
+
+/****************************************************
+ *                    LeafPage                      *
+ ****************************************************
+ *  
+ *  [isLeaf][next leaf pageNum][entries num][entry1][entry2]...
+ * 
+ */
+class LeafPage : public NodePage
+{
+  // [isLeaf][next leaf pageNum][entries num]
+  const static unsigned LEAF_PAGE_HEADER_SIZE = sizeof(bool) + sizeof(unsigned) * 2;
+  vector<LeafEntry *> entries;
+  // 0: no nextPn, since pageNum of meta page is 0
+  PageNum *nextPn;
+
+public:
+  LeafPage(AttrType attrType, InternalPage *parent);
+  LeafPage(char *rawData, AttrType attrType, InternalPage *parent);
+  ~LeafPage();
+
+  RC lookupAndInsert(char *key, unsigned len, RID rid);
+  LeafEntry *lookup(char *key);
+  RC insert(char *key, unsigned len, RID rid);
+
+  RC getRawData(char *data) override;
+  string toString() override;
+};
+
+/****************************************************
+ *                  InternalEntry                   *
+ ****************************************************/
+class InternalEntry
+{
+public:
+  char *key;
+  unsigned size;
+  PageNum ptrNum;
+
+  InternalEntry(char *key, unsigned len, PageNum ptrNum);
+  ~InternalEntry();
+};
+
+/****************************************************
+ *                    LeafEntry                     *
+ ****************************************************/
+class LeafEntry
+{
+public:
+  char *key;
+  unsigned size;
+  RID rid;
+  bool isDeleted;
+
+  LeafEntry(char *key, unsigned len, RID rid, bool isDeleted = false);
+  ~LeafEntry();
+
+  int compareTo(LeafEntry *that, AttrType attrType);
+  string toString(AttrType attrType);
+};
+
 #endif
