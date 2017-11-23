f8f10f55e2851ae2dfac1277d605220908c1c19c
d4rkb1ue <7874364@gmail.com>
2017-11-23 04:02:17 -0800

    feature: pass t6, t7

--- src/ix/t.sh
+++ src/ix/t.sh
@@ -1,2 +1,2 @@
 make clean
-make && ./ixtest_01 && ./ixtest_02 && ./ixtest_03 && ./ixtest_04 && ./ixtest_05 && ./ixtest_06 && ./ixtest_07
+make && ./ixtest_01 && ./ixtest_02 && ./ixtest_03 && ./ixtest_04 && ./ixtest_05 && ./ixtest_06 && ./ixtest_07 && ./ixtest_08
